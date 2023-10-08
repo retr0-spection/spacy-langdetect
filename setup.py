adf5b3f1b8124776fae5cdfc72851f721e5e7940
Rati <ratinailana@icloud.com>
2023-10-08 20:40:31 +0200

    requirements updated

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['pytest', 'langdetect==1.0.7']
+    install_requires=['pytest', 'langdetect==1.0.9']
 )
